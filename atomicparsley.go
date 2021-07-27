22fd3040646d5de599033bfb265ccc45ab4d6f5f
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2021-07-27 15:19:41 +0100

    Update atomicparsley.go

--- atomicparsley.go
+++ atomicparsley.go
@@ -66,11 +66,11 @@ var (
 			"filename":   "AtomicParsleyWindows.exe",
 		},
 		"linux": {
-			"atomicPath": filepath.Join("/var/tmp/", "AtomicParsley"),
+			"atomicPath": filepath.Join("var/tmp/", "AtomicParsley"),
 			"filename":   "AtomicParsleyLinux",
 		},
 		"darwin": {
-			"atomicPath": filepath.Join("/var/tmp/", "AtomicParsley"),
+			"atomicPath": filepath.Join("var/tmp/", "AtomicParsley"),
 			"filename":   "AtomicParsleyMacOS",
 		},
 	}
